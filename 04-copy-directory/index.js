fe495a05224271ed0575c58d9827f0ec6226cd56
julia_chruszczowa <julia.chruszczowa@datylon.svaps.com>
2021-11-07 19:20:26 +0300

    feat: refactor task 4 code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,34 @@
 const fs = require('fs');
 const path = require('path');
-const { readdir } = require('fs/promises');
+const { readdir, mkdir, rm } = require('fs/promises');
 const folderPath = path.resolve(__dirname, './files');
 const copiedPath = path.join(__dirname, './files-copy');
-fs.mkdir(copiedPath, { recursive: true }, (err) => {
-  if (err) console.log(`Error creating directory: ${err}`);
-  // Directory now exists.
-});
-
-async function copyDirectory() {
+async function createFolder(path) {
   try {
-    const files = await readdir(folderPath);
+    await rm(path, { recursive: true });
+    await mkdir(path, { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function copyDirectory(resourceFolder, newFolder) {
+  try {
+    const files = await readdir(resourceFolder);
     files.forEach((file) => {
-      fs.createReadStream(path.join(folderPath, file)).pipe(fs.createWriteStream(path.join(copiedPath, file)));
+      fs.createReadStream(path.join(resourceFolder, file)).pipe(fs.createWriteStream(path.join(newFolder, file)));
     });
   } catch (err) {
     console.error(err);
   }
 }
-copyDirectory();
+async function copyDir(resourceFolder, newFolder) {
+  try {
+    await createFolder(newFolder);
+    await copyDirectory(resourceFolder, newFolder);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir(folderPath, copiedPath);
